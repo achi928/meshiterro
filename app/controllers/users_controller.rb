aa4ed21c8e6882fe338390f92054aefd25e26d89
EC2 Default User <ec2-user@ip-172-31-13-31.ap-northeast-1.compute.internal>
2025-01-14 11:41:25 +0000

    [Finish] bookers2が終わったらレイアウト整える

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -6,10 +9,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
+
     @user = User.find(params[:id])
   end
 
-  def update
+  def update   
+    is_matching_login_user
+
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
@@ -21,4 +28,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+
 end
